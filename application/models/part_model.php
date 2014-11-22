9caf7d992e1430c9cd1e170f890e8db381cd5935
rwhig-1NS <rwhig-1ns@gmail.com>
2014-11-22 15:08:36 -0400

    added to part_model

--- application/models/part_model.php
+++ application/models/part_model.php
@@ -5,7 +5,32 @@ class Part_model extends CI_Model {
 	{
 		$this->load->database();
 	}
-# update quantity
-#retrieve by part_number
+
+	#untested
+
+	public function add_new()
+	{
+		$entry = array();
+		$entry['price'] = $this->input->post('price');
+		$entry['description'] = $this->input->post('description');
+		$entry['quantity'] = $this->input->post('quantity');
+
+		$success = $this->db->insert('part', $entry);
+	}
+
+public function get_parts($number = NULL)
+	{
+		if($number != NULL)
+		{
+			$part = $this->input->post('part_number');
+			$query = $this->db->get_where('part_number', $part);
+		}
+		else
+		{
+			$query = $this->db->get("part");
+		}
+
+		return $query;
+	}
 
 }
