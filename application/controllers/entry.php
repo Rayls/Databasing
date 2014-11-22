6bafa6f9ab77905a8b58aa3db61d98f1436064c2
rwhig-1NS <rwhig-1ns@gmail.com>
2014-11-22 14:22:27 -0400

    initial files

---
+++ application/controllers/entry.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Entry extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->model('part_model');
+		
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/models/part_model.php
@@ -0,0 +1,11 @@
+<?php
+class Part_model extends CI_Model {
+
+	public function __construct()
+	{
+		$this->load->database();
+	}
+# update quantity
+#retrieve by part_number
+
+}
