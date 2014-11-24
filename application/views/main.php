f63c5dbb947936ae857c13df499bbf0e56e87a80
CameronFc <m35191970@gmail.com>
2014-11-24 14:26:31 -0400

    Create main.php

---
+++ application/views/main.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<h1>MUC DBMS</h1>
+	<h2>Navigate with one of the following:<h2>
+	<a href="supplier.php">Add supplier</a>
+	<a href="invoice.php">Add invoice</a>
+	<a href="part.php">Add part</a>
+	<a href=".php">Show Tables</a>
+</body>
+</html>
